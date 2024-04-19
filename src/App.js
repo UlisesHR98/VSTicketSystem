a857a26be10ffb323f22fff0318befa6b96bb7a9
Brandon <brandon.garcia.dev@outlook.com>
2024-04-19 10:52:40 -0600

    Login & Menu pages
    Recommit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import './App.css';
 import "./components/CardsComponent/CardComponent.css"
-import CardComponent from "./components/CardsComponent/CardComponent"
-import { IoTicketOutline } from "react-icons/io5";
-import { FaBus, FaStoreAlt } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
-import Header from './components/HeaderComponent/HeaderComponent';
+import Login from './routes/login';
 
 const App = () => {
 
-  const navigate = useNavigate();
-
-  const handleClick = (path) => {
-    navigate(path);
-  };
-
   return (
-    <div>
-      <Header title="Bienvenido"/>
-      <div className="cards-container">
-        <CardComponent
-          title="Buscar Salidas"
-          content="Buscar asientos disponibles para una fecha específica"
-          Icon={FaBus}
-          onClick={() => handleClick('/departure-searcher')}
-        />
-        <CardComponent
-          title="Buscar Cliente"
-          content="Buscar información de cliente con boleto comprado/apartado"
-          Icon={IoTicketOutline}
-          onClick={() => handleClick('/customer-searcher')}
-        />
-        <CardComponent
-          title="Crear Salida"
-          content="Agregar un autobus disponible para viajar en una fecha y destino específicos"
-          Icon={FaStoreAlt}
-          onClick={() => handleClick('/create-bus')}
-        />
-      </div>
-    </div>
+      <Login />
   );
 };
 
