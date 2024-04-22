aa3e924d9c532e0dc20a95a8f9461073e10c4ea5
mauricio-lna <135794003+mauricio-lna@users.noreply.github.com>
2024-04-22 11:27:13 -0600

    feat [logout]: Logout functionality (#2)
    Logout card component has been added to remove the user login token

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -1,10 +1,14 @@
 import "../components/CardsComponent/CardComponent.css"
 import CardComponent from "../components/CardsComponent/CardComponent"
-import { IoTicketOutline } from "react-icons/io5";
+import { IoTicketOutline, IoLogOut } from "react-icons/io5";
 import { FaBus, FaStoreAlt } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/HeaderComponent/HeaderComponent';
- 
+import Modal from '../components/ModalComponent/ModalComponent';
+import { useState } from "react";
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import LoadingScreen from '../components/LoadingScreenComponent/LoadingScreenComponent';
 
 const Menu = () => {
 
@@ -14,9 +18,39 @@ const Menu = () => {
     navigate(path);
   };
 
+  const handleClickLogout = (path) => {
+    setIsLoading(true);
+    axios.post('http://127.0.0.1:8000/VDSTicketApp/logout/', {
+      username: 'wallc'
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ' + localStorage.getItem('authToken')
+      }
+    }).then(response => {
+      setIsLoading(false);
+      localStorage.removeItem('authToken');
+      navigate(path);
+    })
+      .catch(error => {
+        setIsLoading(false);
+        Swal.fire({
+          title: "Error",
+          text: "No se pudo cerrar sesion",
+          icon: "error",
+          iconColor: "#1ccda0",
+          confirmButtonColor: "#0da290",
+        });
+      });
+
+  };
+
+  const [openModal, setOpenModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div>
-      <Header title="Bienvenido"/>
+      <Header title="Bienvenido" />
       <div className="cards-container">
         <CardComponent
           title="Buscar Salidas"
@@ -36,6 +70,20 @@ const Menu = () => {
           Icon={FaStoreAlt}
           onClick={() => handleClick('/create-bus')}
         />
+        <CardComponent
+          title="Cerrar Sesión"
+          content="Salir del sistema"
+          Icon={IoLogOut}
+          onClick={() => setOpenModal(true)}
+        />
+        {setOpenModal && <Modal
+          title="Atención"
+          content="¿Seguro que quieres salir?"
+          openModal={openModal}
+          closeModal={setOpenModal}
+          onClick={() => handleClickLogout('/Login')}
+        />}
+        {isLoading && <LoadingScreen />}
       </div>
     </div>
   );
